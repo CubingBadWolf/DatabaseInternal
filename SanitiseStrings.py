1945006f3814ddc35d361552856f512b1ec43bfc
CubingBadWolf <gaming1dragons@gmail.com>
2023-05-25 18:29:55 -0700

    idk why that didn't commit before

---
+++ SanitiseStrings.py
@@ -0,0 +1,6 @@
+import re
+
+def SanitiseData(string):
+    '''First attempt. Unresolved'''
+    if not re.match(r'^[a-zA-Z0-9_ ]+$', string):
+        raise ValueError("Invalid sequence string: " + string)
